abc2da046f2a0bf3f629bfc6c22ebb7735fa92dd
Clayton Maciel <62034289+claytonmaciel@users.noreply.github.com>
2021-07-21 11:40:02 -0300

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,4 @@
+var CLIENT_ID = 'UGJcilKxPuO3aLIqtAcXCHXAM2zZVbv1lHslAXH7';
+var REDIRECT_URI = 'http://localhost:8000/teste_login/';
+var SUAP_URL = 'https://suap.ifrn.edu.br';
+var SCOPE = 'identificacao email documentos_pessoais';
